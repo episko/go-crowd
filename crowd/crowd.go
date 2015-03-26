c127b1252740dc998bd155986130f1d3c377c191
Axel Vergult <axel.vergult@gmail.com>
2015-03-26 13:47:23 +0100

    Create HTTP wrapper methods for crowd client

--- crowd/crowd.go
+++ crowd/crowd.go
@@ -1,28 +1,99 @@
 package crowd
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 )
 
 // A Client manages communication with Crowd API.
 type Client struct {
+	// Use HTTP client to interact with API.
+	client *http.Client
+
 	// Application settings
 	name    string
 	passwd  string
 	baseURL *url.URL
-	
+
 	// API methods related to resources are seperated in different services.
 	Users *UsersService
 }
 
 // NewClient returns a new Crowd API client.
-func NewClient(name, passwd, url string) (*Client, error) {
-	baseURL, err := url.Parse(url)
+func NewClient(name, passwd, urlStr string) (*Client, error) {
+	// TODO: check user doesn't yet enter the api path
+	baseURL, err := url.Parse(urlStr + "/crowd/rest/usermanagement/latest")
 	if err != nil {
 		return nil, err
 	}
 
-	c := &Client{name, passwd, baseURL}
+	c := &Client{client: http.DefaultClient, name: name, passwd: passwd, baseURL: baseURL}
 	c.Users = &UsersService{client: c}
 	return c, nil
+}
+
+func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+	u, err := c.baseURL.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	// Encode body as JSON (when present)
+	var buf io.ReadWriter
+	if body != nil {
+		b, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+
+		buf = bytes.NewBuffer(b)
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(c.name, c.passwd)
+
+	return req, nil
+}
+
+// Do sends an API request and returns the API response. The API response is
+// JSON decoded and stored in the value pointed to by v, or returned as an
+// error if an API error has occurred.
+func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	if err := checkHttpResponse(resp); err != nil {
+		return nil, err
+	}
+
+	if v != nil {
+		if w, ok := v.(io.Writer); ok {
+			io.Copy(w, resp.Body)
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(v)
+		}
+	}
+
+	return resp, err
+}
+
+func checkHttpResponse(r *http.Response) error {
+	if c := r.StatusCode; c >= 200 && c <= 299 {
+		return nil
+	}
+
+	return fmt.Errorf("%v %v: %d", r.Request.Method, r.Request.URL, r.StatusCode)
 }
--- crowd/users.go
+++ crowd/users.go
@@ -1,5 +1,13 @@
 package crowd
 
+import (
+	"errors"
+	"fmt"
+	"net/url"
+)
+
+var ErrUnauthorized = errors.New("Unauthorized")
+
 // UsersService handles communication with the user related methods.
 type UsersService struct {
 	client *Client
@@ -13,4 +21,23 @@ type User struct {
 	FirstName   string `json:"first-name"`
 	LastName    string `json:"last-name"`
 	Name        string `json:"name"`
+}
+
+func (s *UsersService) Get(username string) (*User, error) {
+	v := url.Values{}
+	v.Set("username", username)
+	urlPath := "/user?" + v.Encode()
+
+	req, err := s.client.NewRequest("GET", urlPath, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Could not create request: %v", err)
+	}
+	
+	uResp := &User{}
+	_, err = s.client.Do(req, uResp)
+	if err != nil {
+		return nil, fmt.Errorf("Error making http request: %v", err)
+	}
+
+	return uResp, nil
 }
